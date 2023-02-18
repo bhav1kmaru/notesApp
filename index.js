9c3a89682f472bd15c3be8a369418fb840e9f84c
bhav1kmaru <bhavik.m01@gmail.com>
2023-02-18 12:44:14 +0530

    fixed cors

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const swaggerUI=require('swagger-ui-express')
 const swaggerJsDoc = require('swagger-jsdoc')
 
 const app = express()
+app.use(cors());
 app.use(express.json())
-app.use(cors())
+
 
 
 app.get('/',(req,res)=>{
